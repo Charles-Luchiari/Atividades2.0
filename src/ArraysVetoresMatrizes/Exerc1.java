5513b34fad595d4fce65e279bfc2bd588d3f5fcd
Charles <charles99na@gmail.com>
2023-04-18 15:32:28 -0300

    versao4

--- src/ArraysVetoresMatrizes/Exerc1.java
+++ src/ArraysVetoresMatrizes/Exerc1.java
@@ -5,11 +5,26 @@ import java.util.Scanner;
 public class Exerc1 {
 
 	public static void main(String[] args) {
-		
+
 		Scanner leia = new Scanner(System.in);
-		
-		
 
+		int[] vetor = { 2, 5, 1, 3, 4, 9, 7, 8, 10, 6 };
+		int posicao, busca;
+
+		System.out.println("Qual numero você deseja encontrar? ");
+		busca = leia.nextInt();
+		posicao = -1;
+		
+		for (int i = 0; i < vetor.length; i++) {
+			if (vetor[i] == busca) {
+				posicao = i;
+			}
+		}
+
+		if (posicao == -1) {
+			System.out.println("O numero " + busca + " não foi encontrado");
+		} else {
+			System.out.println("O número " + busca + " está na posicao " + posicao);
+		}
 	}
-
 }
---
+++ src/ArraysVetoresMatrizes/Exerc3.java
@@ -0,0 +1,11 @@
+package ArraysVetoresMatrizes;
+
+public class Exerc3 {
+
+	public static void main(String[] args) {
+		
+		int matriz[][] = new int [3][3];
+
+	}
+
+}
